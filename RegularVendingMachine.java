cc157155742337bdd16bda2da57a07c9a4fd1162
rnzbobi <rhenzbhovieviloria@gmail.com>
2023-07-01 00:44:10 +0800

    123

--- RegularVendingMachine.java
+++ RegularVendingMachine.java
@@ -63,7 +63,6 @@ public class RegularVendingMachine {
                 Map<Integer, Integer> change = vendingMachine.dispenseChange(amountPaid, amountToPay);
                 if (change != null) {
                     // Sufficient change available
-
                     // Update quantities and money
                     quantity--;
                     slots.put(ingredient, quantity);
@@ -177,10 +176,10 @@ public class RegularVendingMachine {
         System.out.println("|7| 10 Pesos   |8| 5 Pesos   |9| 1 Pesos");
         System.out.println("|10| EXIT");
         System.out.print("[Enter] ");
+
         do{
             i = Integer.parseInt(sc.nextLine());
-        }while(i < 0 && i > 10);
-
+        }while(i < 0 || i > 10);
         
         if (i == 10) {
             break;
@@ -274,7 +273,6 @@ public class RegularVendingMachine {
             }
         }
     } while (i != 10);
-    
 }
     /**
      * Replenishes the money in the vending machine.
@@ -383,7 +381,7 @@ public class RegularVendingMachine {
                     bank.updateTotalMoney(total);
                 }
             }
-        } while (i != 10 && i < 0 && i >= 11);
+        } while (i != 10);
     }
     /**
      * Displays the starting inventory of the vending machine.
